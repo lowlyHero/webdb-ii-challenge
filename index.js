34b93208047a62d4565e370a4de664b226008af1
lowlyHero <e.nunn1009@gmail.com>
2019-06-25 16:50:40 -0400

    templated out the endpoints in indez

--- index.js
+++ index.js
@@ -6,7 +6,33 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// ============ GET ================
+
+server.get('/zoos', (req, res) => {
+
+});
+
+server.get('/zoos/:id', (req, res) => {
+
+});
+
+// ============ POST ===============
+
+server.post('/zoos', (req, res) => {
+
+});
+
+// ============ PUT ===============
+
+server.put('/zoos/:id', (req, res) => {
+
+});
+
+// ============ DELETE ===============
+
+server.delete('/zoos/:id', (req, res) => {
+
+});
 
 const port = 3300;
 server.listen(port, function() {
